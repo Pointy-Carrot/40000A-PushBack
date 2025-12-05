2b8dcab9a2ce38be8cbc71f7d14454a4b5dbd3da
Pointy-Carrot <139660872+Pointy-Carrot@users.noreply.github.com>
2025-12-05 09:19:04 -0600

    Change planned autos

--- include/misc/autons.h
+++ include/misc/autons.h
@@ -3,10 +3,10 @@
 
 void left_sawp();
 void right_sawp();
-void left_7_ball_long();
-void right_7_ball_long();
 void left_7_ball_split();
 void right_7_ball_split();
+void left_9_ball_split();
+void right_9_ball_split();
 void full_sawp();
 void prog_skills();
 
