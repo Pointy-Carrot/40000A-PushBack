f3d5ef1c157dd510ab4db4921ba69e230db6b796
Pointy-Carrot <139660872+Pointy-Carrot@users.noreply.github.com>
2025-12-08 10:30:18 -0600

    Update robot_init.h

--- include/initialization/robot_init.h
+++ include/initialization/robot_init.h
@@ -16,6 +16,12 @@
 extern DriveExplicit explicitchassis;
 
 // chassis init
+extern lemlib::Drivetrain drivetrain;
+extern lemlib::ControllerSettings lateral_controller;
+extern lemlib::ControllerSettings angular_controller;
+extern lemlib::OdomSensors sensors;
+extern lemlib::TrackingWheel vertical_tracking_wheel;
+extern lemlib::TrackingWheel horizontal_tracking_wheel;
 extern lemlib::Chassis chassis;
 
 // intake init
