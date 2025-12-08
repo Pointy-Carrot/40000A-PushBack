76b5ab9095258eed06ac1637dd25fe4342ff194b
Pointy-Carrot <139660872+Pointy-Carrot@users.noreply.github.com>
2025-12-08 10:13:30 -0600

    Update robot_init.h

--- include/initialization/robot_init.h
+++ include/initialization/robot_init.h
@@ -16,18 +16,10 @@
 extern DriveExplicit explicitchassis;
 
 // chassis init
-extern lemlib::Drivetrain drivetrain;
-extern lemlib::ControllerSettings lateral_controller;
-extern lemlib::ControllerSettings angular_controller;
-extern lemlib::OdomSensors sensors;
-extern lemlib::TrackingWheel vertical_tracking_wheel;
-extern lemlib::TrackingWheel horizontal_tracking_wheel;
 extern lemlib::Chassis chassis;
 
 // intake init
 extern IntakeSubsystem intake;
-extern IntakeState intake_state;
-extern PreviousIntakeState prev_intake_state;
 
 // pneumatics init
 extern PneumaticsSubsystem wing;
@@ -35,20 +27,11 @@ extern PneumaticsSubsystem loader;
 extern PneumaticsSubsystem gate;
 extern PneumaticsSubsystem midgoal_switch;
 
-// scoring system init
-extern Color alliance_color;
-
 // dist reset init
 extern DistanceReset dist_reset;
 
 // cata init
 extern CataSubsystem cata; // after gate and midgoal switch
-extern CataState cata_position;
-
-// trajectory init
-extern Trajectory load_trajectory_from_asset(const char* path_data);
-extern Trajectory path_one;
-extern Trajectory path_two;
 
 // ramsete init
 extern Ramsete ramchassis; // after chassis
